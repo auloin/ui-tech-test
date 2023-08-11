25991839bbacd970017a42a2aa3d881f869a257a
Ismaila Abdoulahi <36740618+auloin@users.noreply.github.com>
2023-08-11 11:27:32 +0200

    Implement field store

--- src/components/fields/useFields.ts
+++ src/components/fields/useFields.ts
@@ -1,54 +1,84 @@
-import { v4 } from "uuid"
+import { v4 } from "uuid";
+import { create } from "zustand";
 
 export type Submit = {
-    type: "submit",
-    text: string,
-    id: string
-    label?: never
-    placeholder?: never
-}
+  type: "submit";
+  text: string;
+  id: string;
+  label?: never;
+  placeholder?: never;
+};
 export type Input = {
-    type: "text" | "date" | "number",
-    text?: never,
-    placeholder: string,
-    id: string,
-    label: string | null
-}
+  type: "text" | "date" | "number";
+  text?: never;
+  placeholder: string;
+  id: string;
+  label: string | null;
+};
 export type Combobox = {
-    type: "country",
-    text?: never,
-    id: string,
-    label: string | null
-    placeholder: string,
-}
-export type Field = Input | Submit | Combobox
-
+  type: "country";
+  text?: never;
+  id: string;
+  label: string | null;
+  placeholder: string;
+};
+export type Field = Input | Submit | Combobox;
 
 const generateRandomUUID = () => {
-    return v4() // this may be replaced with other uuid generators
-}
+  return v4(); // this may be replaced with other uuid generators
+};
 const INIT_FIELDS: Field[] = [
-    { type: "text", placeholder: "Name", label: null, id: generateRandomUUID() },
-    { type: "number", placeholder: "Age", label: null, id: generateRandomUUID() },
-    { type: "date", placeholder: "Date of Birth", label: null, id: generateRandomUUID() },
-    { type: "country", placeholder: "Country", label: null, id: generateRandomUUID() },
-    { type: "submit", text: "submit", id: generateRandomUUID() }
-]
+  { type: "text", placeholder: "Name", label: null, id: generateRandomUUID() },
+  { type: "number", placeholder: "Age", label: null, id: generateRandomUUID() },
+  {
+    type: "date",
+    placeholder: "Date of Birth",
+    label: null,
+    id: generateRandomUUID(),
+  },
+  {
+    type: "country",
+    placeholder: "Country",
+    label: null,
+    id: generateRandomUUID(),
+  },
+  { type: "submit", text: "submit", id: generateRandomUUID() },
+];
 
 /**
  * A store for managing form fields
  * TODO: implement a store for managing form fields
  **/
+interface FieldsState {
+  fields: Field[];
+  update: (field: Partial<Field>, id: string) => void;
+  add: (field: Field) => void;
+  remove: (id: string) => void;
+}
 
+const useFieldsStore = create<FieldsState>((set) => ({
+  fields: INIT_FIELDS,
+  update: (field: Partial<Field>, id: string) =>
+    set(({ fields }) => {
+      const f = fields.find((s) => s.id === id);
+      Object.assign(f as Field, field);
+      return { fields };
+    }),
+  add: (field: Field) =>
+    set(({ fields }) => ({ fields: [...fields, { ...field }] })),
+  remove: (id: string) =>
+    set(({ fields }) => ({
+      fields: fields.filter((field) => field.id !== id),
+    })),
+}));
 
 /**
- * A hook for managing form fields 
+ * A hook for managing form fields
  * TODO: complete this hook by wiring up the form store
- * @returns 
+ * @returns
  */
 function useFields() {
-
-    return { fields: INIT_FIELDS, }
+  return useFieldsStore();
 }
 
 export default useFields;
